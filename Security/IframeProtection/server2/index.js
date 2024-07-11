13b3af5759cb41473e0e25504bf255ea55158f70
Rahul-0108 <53996840+Rahul-0108@users.noreply.github.com>
2024-07-11 21:37:02 +0530

    Update index.js

--- Security/IframeProtection/server2/index.js
+++ Security/IframeProtection/server2/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-    res.setHeader('Content-Security-Policy', "frame-ancestors 'none'")
 
     res.cookie('sessionID', '12345', {
         httpOnly: true,
